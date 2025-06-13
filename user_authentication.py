5dbec470ef8ffc2b006c1f9eb2e14b6b5c4565c1
lomovant <lomov1306@gmail.com>
2025-06-13 06:24:28 +0300

    лаб.1

---
+++ user_authentication.py
@@ -0,0 +1,31 @@
+import hashlib
+
+# Початковий словник користувачів (логін, хешований пароль, ПІБ)
+users = {
+    "alex123": {
+        "password": hashlib.md5("alex_under".encode()).hexdigest(),
+        "full_name": "Lomov Anton"
+    },
+    "user456": {
+        "password": hashlib.md5("petrenko".encode()).hexdigest(),
+        "full_name": "Іван Петренко"
+    }
+}
+
+def check_password(login, input_password):
+    """Перевіряє введений пароль користувача."""
+    if login in users:
+        hashed_input = hashlib.md5(input_password.encode()).hexdigest()
+        if hashed_input == users[login]["password"]:
+            print(f"Вітаємо, {users[login]['full_name']}! Вхід виконано успішно.")
+        else:
+            print("Невірний пароль.")
+    else:
+        print("Користувача не знайдено.")
+
+# Введення даних користувачем
+login = input("Введіть логін: ")
+password = input("Введіть пароль: ")
+
+# Перевірка пароля
+check_password(login, password)
